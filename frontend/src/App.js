050827a628d4c2fe41a2494fabb7e6344570703d
Lex Myers <lex.myers@gmail.com>
2019-01-30 00:54:25 -0800

    starting Django/React code

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Django</h1>
+        </header>
+        <p className="App-intro">
+            A React app with a Django backend
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
